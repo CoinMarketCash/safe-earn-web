ef799220e405de4f1b19b29f6f4dc4fd6a8dfd59
SafeDev <dev@safearn.money>
2021-07-20 20:17:38 -0400

    Website update

--- src/script.js
+++ src/script.js
@@ -3,68 +3,15 @@
 
 
 
-function showModal() {
-	$('#donationModal').modal('show');
-}
-
-function hideModal() {
-	$('#donationModal').modal('hide');
-}
-
-
-
-//This is called when user clicks the first "send donation" button
-const checkMetaAndShowModal = async () => {
-	const metaConnect = await window.ethereum.request({ method: 'eth_requestAccounts' });
-	showModal(); // open donation modal
-}
-
-
-const web3 = new Web3(window.ethereum);
-
-const sendDonation = async (donationValue) => {
-	const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-	await window.ethereum
-		.request({
-			method: 'eth_sendTransaction',
-			params: [
-				{
-					from: accounts[0],
-					to: '0x89141eEFCeAb72dC3425E0bC690f49D91f411fA2',
-					value: parseInt(web3.utils.toWei(donationValue.toString(),"ether")).toString(16)
-				},
-			],
-		}, (err, res) => {
-			console.log(err)
-			console.log(res)
-		});
-
-	$("#thx-for-donate").fadeIn();
-	setTimeout(function() {
-		hideModal();
-		$("#thx-for-donate").hide();
-	}, 2000)
-
-
-}
-
 
 
 
 $(document).ready(function(){
-
-	console.log(window.innerWidth);
-	//$(".overlay").css("background-color","#566467");
-
-	$("#thx-for-donate").hide();
-
-	$("#donateButton").on("click", () => {
-		checkMetaAndShowModal();
-	});
-
-	$(".donation-button").click(function() {
-		sendDonation($(this).data("exact-value"));
-	});
+	$("#arrow-container").on('click', function(e) {
+		console.log("as");
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $("#token-main").offset().top}, 500, 'linear');
+	  });
 });
 
 
